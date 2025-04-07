4c88d5c1959af49c61babca94fd9d18855aeac64
maridabrins <maridabrins@hotmail.com>
2025-04-07 08:35:18 -0300

    feat: add Main in Main.js

---
+++ src/Components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <>
+    <div className='content'>
+        <div className='bg'>
+            <img src='./images/quadrinhos.webp' alt='quadrinhos em preto e branco da Marvel e DC'></img>
+        </div>
+        <div className='container'>
+            <h1>SUPER-HÉROIS</h1>
+            <input type='search' placeholder='Buscar...'></input>
+            <p>TOP 3 FAVORITOS</p>
+        </div>
+    </div>
+    
+    </>
+  )
+}
